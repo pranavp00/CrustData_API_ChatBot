cbe2cf5f8be22cf58e2bf33fe015365facdd6892
pranavp00 <ppillai294@gmail.com>
2025-01-09 21:22:10 +0530

    endpoint changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,14 +1,12 @@
 import axios from 'axios';
 
-// The URL for your Flask backend
-const API_URL = 'http://127.0.0.1:5000/ask';
 
 
 
 export const askQuestion = async (question, chatHistory) => {
   try {
     
-    const response = await axios.post(API_URL, {
+    const response = await axios.post('/ask', {
       question,
       chat_history: chatHistory,
     });
