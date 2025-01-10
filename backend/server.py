f6dbf852ecdc19cfae6967452c3b2c4617c0d36f
pranavp00 <98745390+pranavp00@users.noreply.github.com>
2025-01-10 22:34:01 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -19,7 +19,7 @@ groq_api_key = os.getenv('GROQ_API_KEY')
 app = Flask(__name__)
 
 
-CORS(app, resources={r"/*": {"origins": "https://crustdata-api-chatbot-1.onrender.com/ask"}})
+CORS(app, resources={r"/ask": {"origins": "https://crustdata-api-chatbot-1.onrender.com"}})
 # resources={r"/*": {"origins": "http://localhost:3000"}},
 # supports_credentials=True,
 # methods=["GET", "POST", "OPTIONS"],
